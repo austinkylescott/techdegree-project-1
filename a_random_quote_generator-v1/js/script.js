eb14d0fda51cd1a340ebff32a9c1d66f8668bd63
Austin Scott <austin.scott18@gmail.com>
2019-01-03 14:03:09 -0500

    Added tags, random color bg is working

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -18,32 +18,37 @@ FSJS project 1 - A Random Quote Generator
 
 var quotes = [
   {
-    quote: "hahahaha",
-    source: "John John",
-    citation:"Dictionary"
+    quote: "Unless someone like you cares a whole awful lot nothing's going to get better, it's not.",
+    source: "Dr. Seuss",
+    citation:"Book 'The Lorax'",
+    tags: "Inspiration"
   },
   {
     quote: "You miss 100% of the shots you don't take.",
     source: "Wayne Gretzky",
-    year: 1991
+    year: 1991,
+    tags: "Motivation"
   },
   {
     quote: "To infinity... and beyond!",
     source: "Buzz Lightyear",
     year: 1995,
-    citation:"Movie: 'Toy Story'"
+    citation:"Movie: 'Toy Story'",
+    tags:"Motivation"
   },
   {
     quote: "The ultimate measure of a man is not where he stands in moments of convenience and comfort, but where he stands in moments of challenge and controversy.",
     source: "Martin Luther King, Jr.",
     year: 1963,
-    citation: "Book: 'Strength to Love'"
+    citation: "Book: 'Strength to Love'",
+    tags: "Inspiration"
   },
   {
     quote: "I'm Mary Poppins, y'all!",
     source: "Yondu",
     year: 2017,
-    citation: "Movie: 'Guardians of the Galaxy 2'"
+    citation: "Movie: 'Guardians of the Galaxy 2'",
+    tags: "Joke"
   }
 ];
 
@@ -56,7 +61,7 @@ function getRandomQuote(array) {
 
 //Calls getRandomQuote() and generates HTML required to display quote information
 function printQuote () {
-  var quote = getRandomQuote(quotes);
+  var quote = getRandomQuote(quotes); //sets current quote to randomly selected quote object
   var html = "";
 
   html+= '<p class = "quote">' + quote.quote + ' </p>';   //Adds quote to html string
@@ -76,20 +81,22 @@ function printQuote () {
   console.log(html);
 
   document.getElementById("quote-box").innerHTML = html;
-  document.body.style.backgroundColor = "red";
+  document.body.style.backgroundColor = getRandomColor(); //sets background color to rgb string
+}
+
+function getRandomRGBValue(){
+  return Math.floor(Math.random() * 256);
+}
+
+function getRandomColor () {      //generates RGB values and returns a string
+  var r = getRandomRGBValue();
+  var g = getRandomRGBValue();
+  var b = getRandomRGBValue();
+  return "rgba("+ r +"," + g +","+ b+",1)";
 }
 
 
 
 
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+//Calls printQuote() and changes the quote to our generated HTML
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
